3fda2d692f2789c9b81a72e6c6c4db49bea2afe5
dawidczaja <dawid.czaja5@gmail.com>
2019-04-25 13:34:06 +0200

    create whole tree table

---
+++ create tree_table.sql
@@ -0,0 +1,8 @@
+create table Chapter_73 (
+Id int(10) not null auto_increment primary key, 
+HTS_code varchar(150) not null default'', 
+Lft int(10) not null, 
+Rght int(10) not null,
+Dept int(8) not null 
+ 
+);
