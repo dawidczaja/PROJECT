6fd5bb5261870b5591d5666f2254121789a23ea2
dawidczaja <dawid.czaja5@gmail.com>
2019-04-27 10:39:40 +0200

    zapytania do tree table

---
+++ poziom zagniezdzenia.sql
@@ -0,0 +1,6 @@
+#select depth, hts_code, title from chapter_73 where lft <12 and rght >13;
+#select count(id) as depth from chapter_73 where lft <12 and rght >13;
+#select count(id)-1 as depth from chapter_73 where 12 between lft and rght;
+#select child.hts_code, count(parent.id)-1 as depth from chapter_73 as child, chapter_73 as parent where child.lft between parent.lft and parent.rght group by child.id order by child.lft;
+#select concat(repeat('-', count(parent.id)-1), child.hts_code) as hts_code, child.id from chapter_73 as child, chapter_73 as parent where child.lft between parent.lft and parent.rght group by child.id order by child.lft;
+select hts_code, (select count(parent.id)-1 from chapter_73 as parent where node.lft between parent.lft and parent.rght) as depth from chapter_73 as node order by node.lft;
