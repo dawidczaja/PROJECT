ff2ebdcb33207f3269592ba851a06b71d19b899d
dawidczaja <dawid.czaja5@gmail.com>
2019-04-28 21:09:22 +0200

    addition description to file

--- wyswietlanie galezi.sql
+++ wyswietlanie galezi.sql
@@ -2,5 +2,6 @@ select id, concat(repeat ('-',
 (select count(parent.id)-1 from chapter_73 as parent 
 where node.lft between parent.lft and parent.rght)
 ), node.hts_code) as hts_code, title, lft, rght, depth from chapter_73 as node 
-where node.lft between 3 and 24
+where node.lft between 10 and 23
 order by node.lft;
+# '10' i '23' kolejno left_mark i right_ mark; podajac te liczby wyswietlimy dana czesc gałęzi drzewa jakim jest tabela
