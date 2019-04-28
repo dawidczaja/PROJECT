75d0c973addd5d98fe390fc849ecf0febede097b
dawidczaja <dawid.czaja5@gmail.com>
2019-04-28 21:09:54 +0200

    new file

---
+++ droga.sql
@@ -0,0 +1,2 @@
+select depth, hts_code, title from chapter_73 where lft <=129 and rght >=130;
+#znajacc left_mark i right_mark mozemy wyświetlic kada drogę od korzenia do docelowego 10-cio cyfrowego kodu
